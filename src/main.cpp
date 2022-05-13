b8ba815ee2ea05db84f50a0aa69f58cea2b6cf0c
fishPointer <iantralmer@gmail.com>
2022-05-13 11:41:26 -0700

    Done for now. Everything works as intended :)

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,10 @@
 Adafruit_NeoPixel upline(UPPIXELS, PIN1, NEO_GRBW + NEO_KHZ800);
 
 int light_status = 0;
+int R_val = 0;
+int G_val = 0;
+int B_val = 0;
+int W_val = 0;
 
 uint32_t firstPixelHue = 0;
 
@@ -18,6 +22,10 @@ const char *HUTAO = "192.168.0.193";
 const char *TOPIC_CORN = "corn";
 const char *TOPIC_BOOLY = "booly";
 const char *TOPIC_LUX = "lux";
+const char *TOPIC_RGBW_R = "rgbw/r";
+const char *TOPIC_RGBW_G = "rgbw/g";
+const char *TOPIC_RGBW_B = "rgbw/b";
+const char *TOPIC_RGBW_W = "rgbw/w";
 int mqttPort = 1616;
 
 WiFiClient wifiClient;
@@ -29,6 +37,7 @@ void setupMQTT();
 void reconnect();
 void neo_Rainbow();
 void neo_White();
+void neo_Slide();
 
 ////////////////////////
 
@@ -54,6 +63,10 @@ void loop() {
   if(light_status == 2)
   {
     neo_White();
+  }
+  if(light_status == 3)
+  {
+    neo_Slide();
   }
 
 
@@ -88,6 +101,23 @@ if(strcmp(topic, TOPIC_LUX) == 0)
   callbackmode = 4;
 }
 
+if(strcmp(topic, TOPIC_RGBW_R) == 0)
+{
+  callbackmode = 5;
+}
+if(strcmp(topic, TOPIC_RGBW_G) == 0)
+{
+  callbackmode = 6;
+}
+if(strcmp(topic, TOPIC_RGBW_B) == 0)
+{
+  callbackmode = 7;
+}
+if(strcmp(topic, TOPIC_RGBW_W) == 0)
+{
+  callbackmode = 8;
+}
+
 //Primary switch statement for different functions by topic
 //Master list
   switch(callbackmode)
@@ -95,6 +125,23 @@ if(strcmp(topic, TOPIC_LUX) == 0)
     case 4:
       num_payload = str_payload.toInt();
       light_status = num_payload;
+      break;
+
+    case 5:
+      num_payload = str_payload.toInt();
+      R_val = num_payload;
+      break;
+    case 6:
+      num_payload = str_payload.toInt();
+      G_val = num_payload;
+      break;
+    case 7:
+      num_payload = str_payload.toInt();
+      B_val = num_payload;
+      break;
+    case 8:
+      num_payload = str_payload.toInt();
+      W_val = num_payload;
       break;
 
     //Default Functionality, read off message and topic
@@ -145,6 +192,10 @@ void reconnect() {
         mqttClient.subscribe("potato");
         mqttClient.subscribe("booly");
         mqttClient.subscribe("lux");
+        mqttClient.subscribe("rgbw/r");
+        mqttClient.subscribe("rgbw/g");
+        mqttClient.subscribe("rgbw/b");
+        mqttClient.subscribe("rgbw/w");
       }
       
   }
@@ -169,6 +220,15 @@ void neo_White()
   for(int i=0 ; i<UPPIXELS ; i++)
   {
     upline.setPixelColor(i, upline.Color(0,0,0,255));
+  }
+  upline.show();
+}
+
+void neo_Slide()
+{
+  for(int i=0 ; i<UPPIXELS ; i++)
+  {
+    upline.setPixelColor(i, upline.Color(R_val,G_val,B_val,W_val));
   }
   upline.show();
 }
